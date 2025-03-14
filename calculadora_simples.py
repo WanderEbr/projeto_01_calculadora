75fd49123362fa24f19829ceb8cadff0a3a84a17
wander e bruk <wbrukoski@gmail.com>
2025-03-14 19:50:03 -0300

    prog_01: primeiro commit da calculadora

---
+++ calculadora_simples.py
@@ -0,0 +1,10 @@
+# calcularadora simples com apenas 2 entradas
+# ao final mostra o resultado finalizando o programa
+var_01=0
+var_02=0
+var_resultado=0
+
+var_01=input("digite o primeiro valor: ")
+var_02=input("digite o segundo valor: ")
+var_resultado=int(var_01)+int(var_02)
+print(var_resultado)
